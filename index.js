3d5489225dbf1269b840a5e406f82c3c03bd45de
David Dionise <ddionoise@projectronin.com>
2023-07-12 00:08:52 -0400

    scratch that

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
-const config = require("../config");
+const config = require("./config");
 const cors = require("cors");
-const DeviceEventEmitter = require("../device-event-emitter");
+const DeviceEventEmitter = require("./device-event-emitter");
 
 const app = express();
 const deviceEventEmitter = new DeviceEventEmitter();
